593619ec7af2c2ef4e7f73df477bcdb8de1085e9
Rishit R Sanghani <rishit.code.dev.2010@gmail.com>
2026-01-17 12:04:26 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = input("Enter the name of the city you were born in: \n")
+animal = input("Enter the name of your favourite animal: \n")
+print("The name of your band is: " + city + " " + animal)
